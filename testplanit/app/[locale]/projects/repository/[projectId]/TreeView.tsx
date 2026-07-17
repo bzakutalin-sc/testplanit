bfd5cef97844a527b450e0f371294a93b361b804
Boris Zakutalin <bozak@scand.com>
2026-07-17 15:52:55 +0300

    #2: Fix collapse of Menu in TreeView.tsx

--- testplanit/app/[locale]/projects/repository/[projectId]/TreeView.tsx
+++ testplanit/app/[locale]/projects/repository/[projectId]/TreeView.tsx
@@ -1182,6 +1182,7 @@ const TreeView: React.FC<{
                   className="h-7 w-7 p-0"
                   data-testid={`folder-actions-trigger-${data?.folderId ?? 0}`}
                   aria-label={t("common.actions.actionsLabel")}
+                  onClick={(e) => e.stopPropagation()}
                 >
                   <MoreVertical className="h-5 w-5" />
                 </Button>
